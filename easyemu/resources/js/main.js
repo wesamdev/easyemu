33e80ddc79fb6153693f647615575f9beb590723
wesam <software.market999@gmail.com>
2024-03-17 15:14:07 +0400

    ////

--- easyemu/resources/js/main.js
+++ easyemu/resources/js/main.js
@@ -1,16 +1,4 @@
-// This is just a sample app. You can structure your Neutralinojs app code as you wish.
-// This example app is written with vanilla JavaScript and HTML.
-// Feel free to use any frontend framework you like :)
-// See more details: https://neutralino.js.org/docs/how-to/use-a-frontend-library
-
-function showInfo() {
-    document.getElementById('info').innerHTML = `
-        ${NL_APPID} is running on port ${NL_PORT}  inside ${NL_OS}
-        <br/><br/>
-        <span>server: v${NL_VERSION} . client: v${NL_CVERSION}</span>
-        `;
-}
-
+//main.js
 function openDocs() {
     Neutralino.os.open("https://neutralino.js.org/docs");
 }
